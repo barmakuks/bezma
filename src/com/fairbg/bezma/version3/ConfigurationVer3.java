726fcacae74e3835bb6c66db8fa53479324d1892
vitalii misiura <vitalii.misiura@gmail.com>
2013-10-19 13:21:18 +0300

    Added Doxygen documents generation

--- src/com/fairbg/bezma/version3/ConfigurationVer3.java
+++ src/com/fairbg/bezma/version3/ConfigurationVer3.java
@@ -6,6 +6,6 @@ import com.fairbg.bezma.core.Configuration;
 public class ConfigurationVer3 extends Configuration
 {
     private static final long serialVersionUID = -2509360009329099420L;
-//    public String deviceMAC = "00:12:6F:22:46:2C";
-    public String deviceMAC = "00:12:6F:22:45:98";
+    public String deviceMAC = "00:12:6F:22:46:2C";
+//    public String deviceMAC = "00:12:6F:22:45:98";
 }
