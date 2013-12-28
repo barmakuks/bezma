0bb2ca671a57a159d7a0decf2e151707a226c294
Vitalii <Vitalii@localhost>
2013-12-28 23:59:46 +0200

    Remove files

--- src/com/fairbg/bezma/core/model/IGameAutomat.java
+++
@@ -1,9 +0,0 @@
-package com.fairbg.bezma.core.model;
-
-import com.fairbg.bezma.core.backgammon.Position;
-
-public interface IGameAutomat
-{
-    boolean processCommand(IGameBox gameBox, ModelCommand modelCommand);
-    Position getCurrentPosition();
-}
